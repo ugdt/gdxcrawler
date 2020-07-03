6ba42af39e1c5c5464cfc912999ab526f64dca34
Andrew Bueide <andrew@abueide.com>
2020-07-03 01:18:45 -0600

    First commit

---
+++ desktop/src/main/java/com/abysl/gdxcrawler/desktop/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package com.abysl.gdxcrawler.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.abysl.gdxcrawler.Main;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("gdxcrawler");
+		configuration.setWindowedMode(640, 480);
+		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		return configuration;
+	}
+}
