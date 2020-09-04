598f477dcd9a5e1d0eb3ea8ebfe19bbaf3b45b79
Emery Tanghanwaye <emerytang@gmail.com>
2020-09-03 19:47:01 -0500

    gdxcrawler: tilecollision stubs

---
+++ core/src/main/kotlin/com/abysl/gdxcrawler/ecs/events/TileCollisionEvent.kt
@@ -0,0 +1,12 @@
+package com.abysl.gdxcrawler.ecs.events
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
+import com.badlogic.gdx.math.GridPoint2
+import com.badlogic.gdx.math.Vector2
+
+class TileCollisionEvent(
+        val entityId: Int,
+        val position: Vector2,
+        val tile: Pair<GridPoint2, TiledMapTileLayer.Cell>,
+) : Event() {
+}
---
+++ core/src/main/kotlin/com/abysl/gdxcrawler/ecs/systems/TileSystem.kt
@@ -0,0 +1,9 @@
+package com.abysl.gdxcrawler.ecs.systems
+
+import com.artemis.systems.IteratingSystem
+
+class TileSystem : IteratingSystem() {
+    override fun process(entityId: Int) {
+
+    }
+}
